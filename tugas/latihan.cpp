5931c3b4cd2f8d0200796412de8f5e02be29f59e
root <root@LAPTOP-TB8AB661.localdomain>
2021-10-08 12:39:11 +0700

    test

---
+++ tugas/latihan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka;
+    cout<<"Masukkan Angka : "; 
+    cin>>angka;
+    if(angka%2 == 1){
+        cout<<"Gajil";
+    }else{
+        cout<<"Genap";
+    }
+}
