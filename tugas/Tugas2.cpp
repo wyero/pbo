dfbd2052cac86723199e8231299365b3a4cb82a1
root <root@LAPTOP-TB8AB661.localdomain>
2021-10-30 09:44:02 +0700

    test

--- tugas/Tugas2.cpp
+++ tugas/Tugas2.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 using namespace std;
 
+class waktu{
+	private :
+		int jam, menit, detik;
+	public :
+		waktu(int j, int m, int d);
+		int hitung(int j, int m, int d);
+		int tambahJam();
+		int tambahMenit();
+		int tambahDetik();
+		void hasil();
+};
+waktu::waktu(int jam, int menit, int detik){
+	hitung(jam, menit, detik);
+}
+int waktu::hitung(int j, int m, int d){
+	if((j>=0 && j<=59)&&(m>=0 && m<=59)&&(d>=0 && d<=59)){
+		jam = j;
+		menit = m;
+		detik = d;
+	}else{
+		cout<<"format salah"<<endl;
+	}
+}
+	int waktu::tambahJam(){
+		return jam;
+	}
+	int waktu::tambahMenit(){
+		return menit;
+	}
+	int waktu::tambahDetik(){
+		return detik;
+	}
+	void waktu::hasil(){
+		cout<<jam<<" : "<<menit<<" : "<<detik;
+}
 int main(){
-    int thnLhr, thnSkrng, umur;
-    cout<<"Masukkan Tahun Lahir : ";
-    cin>>thnLhr;
-    cout<<"Masukkan Tahun Sekarang : ";
-    cin>>thnSkrng;
-    umur = thnSkrng - thnLhr;
-    cout<<"Berarti usia anda sekarang adalah "<<umur<<" tahun";
+	waktu w;
+	w.hitung(60,60,60);
+	w.hasil();
 }
--- tugas/tgs6.cpp
+++ tugas/tgs6.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 class waktu{
-	private : // data hanya dapat diakses dalam kelas dan tidak bisa diakses diluar kelas		   
+	private : //data hanya dapat diakses dalam kelas dan tidak bisa diakses diluar kelas
 		int jam, menit, detik;
-	public : // data bisa diakses di bagian program mana saja	
+	public : //data bisa diakses dibagian program mana saja
 		void hitung(int j, int m, int d){
-			if((j>=0 && j<=23)&&(m>=0 && m<=59)&&(d>=0 && d<=59)){
+			if((j>=0 && j<=23)&&(m>=0 && m<=59)&&(d>=0&&d<=59)){
 				jam = j;
 				menit = m;
 				detik = d;
 			}else{
-				cout<<"format waktu salah"<<endl;
+				cout<<"format jam salah"<<endl;
 			}
 		}
 		int tambahJam(){
@@ -24,11 +24,11 @@ class waktu{
 			return detik;
 		}
 		int atur(){
-			cout<<jam<<" : "<<menit<<" : "<<detik;
+			cout<<jam<<" : "<<menit<<" : "<<detik<<endl;
 		}
 };
 int main(){
-	waktu w;
-	w.hitung(11,59,10);
+	waktu w;	
+	w.hitung(12,30,20);
 	w.atur();
 }
